bfa8eacbbb0a82f473c7cc1e98f31ebc28eb2249
June Shaw <june.shaw@me.com>
2017-07-30 11:57:16 -0600

    Adding lists of post from blog api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export FETCH_POSTS = 'fetch_posts';
+export const FETCH_POSTS = 'fetch_posts';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
 const API_KEY = '?key=JUNEBUG123';
 
-export function fetch_posts() {
+export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return {
     type: FETCH_POSTS,
