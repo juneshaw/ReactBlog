87b0ae77838a67efd1353b253ccd4c9bea40e870
June Shaw <june.shaw@me.com>
2017-07-31 20:05:17 -0600

    Adding field to form

---
+++ src/components/posts_new.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+  renderTitleField(field) {
+    return(
+      <div>
+        <input
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+  render () {
+    console.log("got to posts new");
+    return (
+      <div>
+        <form>
+          <Field
+            name="title"
+            component={this.renderTitleField}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
