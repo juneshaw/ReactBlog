eded52c08e153ba5db98636a85888957cc5ea235
June Shaw <june.shaw@me.com>
2017-08-01 20:47:18 -0600

    Adding form to posts new page

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return(
-      <div>
-        <input
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control"
           type="text"
           {...field.input}
         />
@@ -13,13 +14,23 @@ class PostsNew extends Component {
     )
   }
   render () {
-    console.log("got to posts new");
     return (
       <div>
         <form>
           <Field
+            label="Title"
             name="title"
-            component={this.renderTitleField}
+            component={this.renderField}
+          />
+          <Field
+            label="Categories"
+            name="categories"
+            component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
           />
         </form>
       </div>
@@ -27,6 +38,24 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters in length";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content please";
+  }
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
