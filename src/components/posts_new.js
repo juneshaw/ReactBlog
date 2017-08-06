ece07c78cc676111cca49f5411c67e5fb0ea5fe9
June Shaw <june.shaw@me.com>
2017-08-06 10:17:02 -0600

    Styling form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
   renderField(field) {
+    const {meta: {touched, error}, label, input } = field;
+    // const {label, input} = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
     return(
-      <div className="form-group">
-        <label>{field.label}</label>
+      <div className={className}>
+        <label>{label}</label>
         <input className="form-control"
           type="text"
-          {...field.input}
+          {...input}
         />
-        {field.meta.error}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
@@ -21,6 +28,7 @@ class PostsNew extends Component {
 
   render () {
     const { handleSubmit } = this.props;
+
     return (
       <div>
         <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
