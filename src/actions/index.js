6eaef81f0fc432bed8ae5ccd304a4c7276f9e4d5
June Shaw <june.shaw@me.com>
2017-08-15 21:58:35 -0600

    delete post

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
 const API_KEY = '?key=JUNEBUG123';
 
@@ -15,7 +16,6 @@ export function fetchPosts() {
 }
 
 export function createPost(values, callback) {
-  console.log('inside createPost:', values);
   const request = axios.post(`${ROOT_URL}posts${API_KEY}`, values)
     .then(() => callback());
   return {
@@ -29,5 +29,14 @@ export function fetchPost(id) {
   return {
     type: FETCH_POST,
     payload: request
-  }
+  };
+}
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback());
+  return {
+    type: DELETE_POST,
+    payload: id
+  };
 }
